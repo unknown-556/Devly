2ee86f68527c199ef90d9c498909658f2739f336
unknown-556 <obasio1115@gmail.com>
2024-07-17 16:31:48 +0100

    login

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -76,9 +76,7 @@ export const signIn = async (req, res, next) => {
         
         const comparePass = comparePasswords(password, user.password);
         if (!comparePass) {
-            console.log({ message: 'Password is incorrect' });
-            return res.json({message: 'password is incorrect'})
-            return res.status(400).json({message:'Password is incorrect'})
+            return
         }
         
         
